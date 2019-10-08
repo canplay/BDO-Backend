ebacd0a5e1a3d97331988d13aa17f9c9b16e2786
CaNplay <canplay@vip.qq.com>
2019-10-08 23:16:03 +0800

    #1

--- route.js
+++ route.js
@@ -11,170 +11,96 @@ async function routes(fastify, options) {
   const svgCaptcha = require("svg-captcha");
   const bcrypt = require("bcrypt");
 
-  let prefix = 0;
-  let userList = [];
-
-  // removeUser();
-
-  fastify.get("/home/:type", async (request, reply) => {
+  fastify.get("/home/:type", async (req, reply) => {
     let value;
 
-    switch (request.params.type) {
+    switch (req.params.type) {
       case "news":
-          value = await newsCol.find();
-          break;
+        value = await newsCol.find();
+        break;
       case "slide":
-          value = await slideCol.find();
-          break;
+        value = await slideCol.find();
+        break;
       case "config":
-          value = await configCol.find();
-          break;
+        value = await configCol.find();
+        break;
     }
 
-    if (value === null) reply.code(404);
-      
-    value.toArray(function(err, result) {
-      if (err) throw err;
-      reply.send(result);
-    })
+    if (value === null) return { status: 0 };
+
+    let result = await value.toArray();
+    return { status: 1, msg: result };
   });
 
-  fastify.get("/markdown", async (request, reply) => {
+  fastify.get("/markdown", async (req, reply) => {
     let value = await markdownCol.find();
 
-    if (value === null) reply.code(404);
-    
-    value.toArray(function(err, result) {
-      if (err) throw err;
-      reply.send(result);
-    })
+    if (value === null) return { status: 0 };
+
+    let result = await value.toArray();
+    return { status: 1, msg: result };
   });
 
-  fastify.get("/news/:id", async (request, reply) => {
-    let ObjectId = require('mongodb').ObjectId;
+  fastify.get("/news/:id", async (req, reply) => {
+    let ObjectId = require("mongodb").ObjectId;
 
-    let value = await newsCol.findOne({ _id: ObjectId(request.params.id) });
+    let value = await newsCol.findOne({ _id: ObjectId(req.params.id) });
 
-    if (value === null) reply.code(404);
-    
-    reply.send(value);
+    if (value === null) reply.code(404).send();
+
+    return { status: 1, msg: value };
   });
 
-  fastify.get("/captcha", async (request, reply) => {
+  fastify.get("/captcha", async (req, reply) => {
     let captcha = svgCaptcha.create();
-    let id = Date.now() + (prefix++);
+    return { status: 1, msg: captcha.text };
+  });
 
-    userList.push({
-      id: id,
-      captcha: captcha.text
+  fastify.post("/login", async (req, reply) => {
+    let value = await accountsCol.findOne({
+      accountName: req.body.username
     });
 
-    return {
-      id: id,
-      captcha: captcha.text
-    };
-  });
-
-  function check(id, captcha) {
-    if (userList.length <= 0) {
-      return {
-        id: id,
-        status: "id error"
-      };
-    }
-
-    for (let index = 0; index < userList.length; index++) {
-      if (userList[index].id === parseInt(id)) {
-        if (userList[index].captcha != captcha) {
-          return {
-            id: id,
-            status: "captcha error"
-          };
-        }
-
-        return {
-          id: id,
-          status: "success"
-        };
-      }
-    }
-
-    return {
-      id: id,
-      status: "id error"
-    };
-  }
-
-  fastify.get("/register/:username/:password", async (request, reply) => {
-    // let result = check(request.params.id, request.params.captcha);
-
-    // if (result.status === "success") {
-      let value = await accountsCol.findOne({ accountName: request.params.username })
-
-      if (value != null) return "username exit";
-
-      let col = await accountsCol.find().sort({ _id: -1 });
-      // let index = 0;
-      // col.forEach(item => {
-      //   if (index < item._id) index = item._id;
-      // });
-
-      let long = require("mongodb").Long;
-      let account = require("./account.json");
-
-      account._id = long.fromInt(await col.count() + 1);
-      account.accountName = account.email = request.params.username;
-
-      let salt = bcrypt.genSaltSync(10, "a");
-      account.password = bcrypt.hashSync(request.params.password, salt);
-
-      account.cash = long.fromInt(0);
-      account.registrationDate = long.fromNumber(Date.now());
-      account.host = request.ip;
-
-      value = await accountsCol.insertOne(account);
-
-      if (value == null) return "failed";
-      else if (!value.result.ok) return "failed";
-
-      userList.pop();
-
-      return "success";
-    // }
-
-    // return result.status;
-  });
-
-  fastify.get("/login/:username/:password", async (request, reply) => {
-    let value = await accountsCol.findOne({ accountName: request.params.username });
-
     if (value != null) {
-      if (!bcrypt.compareSync(request.params.password, value.password))
-        return "password error";
-      return "success";
+      if (!bcrypt.compareSync(req.body.password, value.password))
+        return { status: 0, msg: "password error"};
+
+      const uuid = require("uuid/v4");
+      return { status: 1, msg: uuid() + "-" + uuid() };
     }
 
-    return "username error";
+    return { status: 0, msg: "username error" };
   });
 
-  fastify.get("/admin/:info", async (request, reply) => {
-    let value = await accountsCol.findOne({ accountName: request.params.username });
+  fastify.post("/register", async (req, reply) => {
+    let value = await accountsCol.findOne({
+      accountName: req.body.username
+    });
 
-    if (value != null) {
-      if (!bcrypt.compareSync(request.params.password, value.password))
-        return "password error";
-      return "success";
-    }
+    if (value != null) return { status: 0, msg: "username exit" };
 
-    return "username error";
+    let col = await accountsCol.find().sort({ _id: -1 });
+
+    let long = require("mongodb").Long;
+    let account = require("./account.json");
+
+    account._id = long.fromInt((await col.count()) + 1);
+    account.accountName = account.email = req.body.username;
+
+    let salt = bcrypt.genSaltSync(10, "a");
+    account.password = bcrypt.hashSync(req.body.password, salt);
+
+    account.cash = long.fromInt(0);
+    account.registrationDate = long.fromNumber(Date.now());
+    account.host = req.ip;
+
+    value = await accountsCol.insertOne(account);
+
+    if (value == null) return { status: 0 };
+    else if (!value.result.ok) return { status: 0 };
+
+    return { status: 1, msg: "success" };
   });
-
-  function removeUser() {
-    console.log(userList);
-    userList.pop();
-    console.log(userList);
-    setTimeout(removeUser, 18000);
-  }
 }
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 const fastify = require("fastify")({
   logger: true
 });
 
-fastify.register(require('fastify-static'), {
+fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "web")
-})
+});
+fastify.register(require("fastify-compress"));
+fastify.register(require("fastify-cors"), {
+  origin: ["http://localhost:8080", "http://127.0.0.1:3000"]
+});
+
+// fastify.register(require('fastify-oauth2'), {
+//   name: 'facebookOAuth2',
+//   credentials: {
+//     client: {
+//       id: '<CLIENT_ID>',
+//       secret: '<CLIENT_SECRET>'
+//     },
+//     auth: require('fastify-oauth2').FACEBOOK_CONFIGURATION
+//   },
+// register a fastify url to start the redirect flow
+// startRedirectPath: '/login/facebook',
+// facebook redirect here after the user login
+//   callbackUri: 'http://localhost:3000/login/facebook/callback'
+// });
 
 fastify.register(require("./db"), {
   url: "mongodb://127.0.0.1:27017/",
@@ -14,11 +33,8 @@ fastify.register(require("./db"), {
   useUnifiedTopology: true
 });
 fastify.register(require("./route"));
-fastify.register(require("fastify-cors"), {
-  origin: [ "http://localhost:8080", "http://127.0.0.1:3000" ]
-});
 
-fastify.listen(3000, "0.0.0.0", function (err, address) {
+fastify.listen(3000, "0.0.0.0", function(err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
